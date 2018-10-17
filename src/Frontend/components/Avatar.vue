4e4999c8ffbdbb2a0d2d8ed01aa3c4e10665e5a2
stupid cat <cat@blargbot.xyz>
2018-10-17 09:14:12 -0600

    avatars

---
+++ src/Frontend/components/Avatar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <figure>
+      <img @mouseover='mouseOver' :src='initial' alt='a lovely avatar!'>
+      <figcaption>Avatar #{{this.avatarId}}</figcaption>
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['avatarId'],
+  data: () => ({
+    initial: '/img/avatars/placeholder.png',
+  }),
+  methods: {
+    mouseOver() {
+      this.initial = `/img/avatars/${this.avatarId}.${this.avatarId < 8 ? 'png' : 'gif'}`;
+    },
+  },
+};
+
+</script>
+
+<style lang="scss" scoped>
+figure {
+  margin: 0;
+  figcaption {
+    text-align: center;
+  }
+  img {
+    width: 250px;
+    height: 250px;
+  }
+}
+</style>
---
+++ src/Frontend/pages/avatars.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <section>
+      <h1>Avatars</h1>
+      <p>This is a collection of all the avatars I've made and used. For your convenience, images will only
+        <!-- -->display when you hover over them!</p>
+    </section>
+    <div class='flexbox'>
+      <avatar v-for='n in total' :key='n' :avatar-id='n' />
+    </div>
+  </main>
+</template>
+
+<script>
+import Avatar from '~/components/Avatar.vue';
+
+export default {
+  data: () => ({
+    total: 30,
+  }),
+  components: { Avatar },
+};
+</script>
+
+<style lang="scss" scoped>
+.flexbox {
+  display: flex;
+  flex-wrap: wrap;
+  & > * {
+    margin: 15px;
+  }
+}
+</style>
